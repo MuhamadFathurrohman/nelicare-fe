2e7082dec273f2d31d06f01a9f1cb2317cdffe44
MuhamadFathurrohman <muhamadfathurrohman05@gmail.com>
2025-05-26 15:53:42 +0700

    fix router

--- src/router.jsx
+++ src/router.jsx
@@ -1,5 +1,4 @@
 import { createBrowserRouter, Navigate } from "react-router-dom";
-import Dashboard from "./Dashboard.jsx";
 import DefaultLayout from "./components/DefaultLayout";
 import GuestLayout from "./components/GuestLayout";
 import Login from "./views/Login";
@@ -14,70 +13,30 @@ import Kontak from "./views/User/Kontak.jsx";
 import HomeUser from "./views/User/Homepage/HomeUser.jsx";
 
 const router = createBrowserRouter([
-    {
-        path: "/",
-        element: <HomeUser />
-    },
-    {
-        path: "/buat-janji",
-        element: <BuatJanji />
-    },
-    {
-        path: "/jadwal",
-        element: <Jadwal />
-    },
-    {
-        path: "/edukasi",
-        element: <Edukasi />
-    },
-    {
-        path: "/kontak",
-        element: <Kontak />
-    },
-    {
-        path: "/admin/section",
-        element: <DefaultLayout />,
-        children: [
-            {
-                path: "/admin/section/",
-                element: <Navigate to="/dashboard" />,
-            },
-            {
-                path: "/admin/section/dashboard",
-                element: <Dashboard />,
-            },
-            {
-                path: "/admin/section/users",
-                element: <Users />,
-            },
-            {
-                path: "/admin/section/users/new",
-                element: <UserForm key="userCreate" />,
-            },
-            {
-                path: "/admin/section/users/:id",
-                element: <UserForm key="userUpdate" />,
-            },
-        ],
-    },
-    {
-        path: "/guest",
-        element: <GuestLayout />,
-        children: [
-            {
-                path: "/guest/login",
-                element: <Login />,
-            },
-            {
-                path: "/guest/signup",
-                element: <Signup />,
-            },
-        ],
-    },
-    {
-        path: "*",
-        element: <NotFound />,
-    },
+  {
+    path: "/",
+    element: <HomeUser />,
+  },
+  {
+    path: "/buat-janji",
+    element: <BuatJanji />,
+  },
+  {
+    path: "/jadwal",
+    element: <Jadwal />,
+  },
+  {
+    path: "/edukasi",
+    element: <Edukasi />,
+  },
+  {
+    path: "/kontak",
+    element: <Kontak />,
+  },
+  {
+    path: "*",
+    element: <NotFound />,
+  },
 ]);
 
 export default router;
