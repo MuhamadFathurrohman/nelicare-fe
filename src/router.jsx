26b084fe26cdf528ec15c6c45ec12aebc8a20f29
MuhamadFathurrohman <muhamadfathurrohman05@gmail.com>
2025-05-26 16:36:39 +0700

    edit router

--- src/router.jsx
+++ src/router.jsx
@@ -1,11 +1,5 @@
 import { createBrowserRouter, Navigate } from "react-router-dom";
-import DefaultLayout from "./components/DefaultLayout";
-import GuestLayout from "./components/GuestLayout";
-import Login from "./views/Login";
 import NotFound from "./views/NotFound";
-import Signup from "./views/Signup";
-import Users from "./views/Users";
-import UserForm from "./views/UserForm";
 import BuatJanji from "./views/User/BuatJanji.jsx";
 import Jadwal from "./views/User/Jadwal.jsx";
 import Edukasi from "./views/User/Edukasi.jsx";
